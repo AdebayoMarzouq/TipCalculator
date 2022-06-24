20e577798acf7e60025317cb2f0cac8a9c1a13d5
Marzouq <marzouqdevmail@gmail.com>
2022-06-24 09:44:40 +0100

    complete tip calculator app

--- README-template.md
+++ README-template.md
@@ -1,5 +1,7 @@
 # Frontend Mentor - Tip calculator app solution
 
+### Dont Mind my readme, would update later :)
+
 This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
 ## Table of contents
@@ -34,7 +36,7 @@ Users should be able to:
 
 Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
 
-Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it. 
+Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it.
 
 Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
 
@@ -52,13 +54,7 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 - Semantic HTML5 markup
 - CSS custom properties
 - Flexbox
-- CSS Grid
 - Mobile-first workflow
-- [React](https://reactjs.org/) - JS library
-- [Next.js](https://nextjs.org/) - React framework
-- [Styled Components](https://styled-components.com/) - For styles
-
-**Note: These are just examples. Delete this note and replace the list above with your own choices**
 
 ### What I learned
 
@@ -69,11 +65,13 @@ To see how you can add code snippets, see below:
 ```html
 <h1>Some HTML code I'm proud of</h1>
 ```
+
 ```css
 .proud-of-this-css {
   color: papayawhip;
 }
 ```
+
 ```js
 const proudOfThisFunc = () => {
   console.log('🎉')
--- script.js
+++ script.js
@@ -1,16 +1,121 @@
-// const bill = document.getElementById('billInput')
-// const people = document.getElementById('peopleInput')
-// const percentages = document.getQuerySelectorAll('.tip-content .btn')
+const bill = document.getElementById('billInput')
+const people = document.getElementById('peopleInput')
+const customTipPercent = document.getElementById('customInput')
+const tipPercent = document.querySelectorAll('.btn-group input[type="button"')
+const tipInputs = document.querySelectorAll('input[type="number"')
+const updateVals = document.querySelectorAll('.tip-amount')
+const reset = document.getElementById('reset')
 
-const defaultPercent = 0
+let globalPercent = 0
+let globalBill = 0
+let globalPeople = 0
+let error = false
+let errorArray = []
+
+resetVals()
+
+window.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter' && error) {
+    console.log('cannot evalutate, erorr')
+    return
+  }
+  calculateTip(globalBill, globalPercent, globalPeople)
+})
+
+tipPercent.forEach((element) => {
+  element.addEventListener('click', (e) => {
+    globalPercent = +e.target.dataset.value
+    if (error) {
+      console.log('cannot evalutate, erorr')
+      return
+    }
+    calculateTip(globalBill, globalPercent, globalPeople)
+  })
+})
+
+bill.addEventListener('input', (e) => {
+  let check = validate(e.target, +e.target.value)
+  if (check) {
+    return
+  }
+  globalBill = +e.target.value
+  if (error) {
+    console.log('cannot evalutate, erorr')
+    return
+  }
+  calculateTip(globalBill, globalPercent, globalPeople)
+})
+
+people.addEventListener('input', (e) => {
+  let check = validate(e.target, +e.target.value)
+  if (check) {
+    return
+  }
+  globalPeople = +e.target.value
+  if (error) {
+    console.log('cannot evalutate, erorr')
+    return
+  }
+  calculateTip(globalBill, globalPercent, globalPeople)
+})
+
+customTipPercent.addEventListener('input', (e) => {
+  let check = validate(e.target, +e.target.value)
+  if (check) {
+    return
+  }
+  globalPercent = +e.target.value
+  if (error) {
+    console.log('cannot evalutate, erorr')
+    return
+  }
+  calculateTip(globalBill, globalPercent, globalPeople)
+})
+
+reset.addEventListener('click', () => resetVals())
 
 function calculateTip(bill, percent, people) {
+  if (error) {
+    return
+  }
   let tipAmount = (parseFloat(bill) / 100) * parseFloat(percent)
-
   let result = (parseFloat(bill) + tipAmount) / parseInt(people)
-
-  return [(tipAmount / people).toFixed(2), result.toFixed(2)]
+  if (tipAmount && result && isFinite(result)) {
+    updateTip((tipAmount / people).toFixed(2), result.toFixed(2))
+  }
+  return
 }
 
-let [ans, ansb] = calculateTip(142.55, 15, 5)
-console.log(ans, ansb)
+function updateTip(tipPerPerson, billPerPerson) {
+  updateVals[0].innerText = `$${tipPerPerson}`
+  updateVals[1].innerText = `$${billPerPerson}`
+}
+
+function resetVals() {
+  bill.value = ''
+  people.value = ''
+  customTipPercent.value = ''
+  updateVals[0].innerText = '$0.00'
+  updateVals[1].innerText = '$0.00'
+  globalPercent = 0
+  globalBill = 0
+  globalPeople = 0
+  error = false
+}
+
+function validate(element, data) {
+  let label = document.querySelector(`label[for=${element.id}] span.error`)
+  console.log(label)
+  if (data <= 0 || '' || !isFinite(data)) {
+    error = true
+    element.classList.remove('valid')
+    element.classList.add('invalid')
+    label.style.visibility = 'visible'
+    return true
+  }
+  error = false
+  element.classList.remove('invalid')
+  element.classList.add('valid')
+  label.style.visibility = 'hidden'
+  return false
+}
